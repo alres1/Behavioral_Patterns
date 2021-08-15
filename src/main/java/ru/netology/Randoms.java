6a46827f7eae21314db6931f01bd35de3c232b2d
Alres <lexsparrow@yandex.ru>
2021-08-15 22:09:53 +0300

    first commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+    private final int min;
+    private final int max;
+    protected Random random;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.ints(min, max+1)
+                        .findFirst()
+                        .getAsInt();
+            }
+        };
+    }
+
+}
